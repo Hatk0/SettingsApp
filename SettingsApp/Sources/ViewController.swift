508c8db95787dbcc482479a8900b5744cfa25b18
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-01 14:55:28 +0300

    Project customization

---
+++ SettingsApp/Sources/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        
+    }
+
+    private func setupLayout() {
+        
+    }
+
+    // MARK: - Actions
+    
+}
+
