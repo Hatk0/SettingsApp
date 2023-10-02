e8b391ef5629749bfe1bdeab227d8982d7103b68
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-02 17:54:40 +0300

    Basic table setup

--- SettingsApp/Sources/ViewController.swift
+++ SettingsApp/Sources/ViewController.swift
@@ -5,11 +5,20 @@ class ViewController: UIViewController {
     
     // MARK: - Outlets
     
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        return tableView
+    }()
+    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .white
+        title = "Settings"
+        navigationController?.navigationBar.prefersLargeTitles = true
         setupHierarchy()
         setupLayout()
     }
@@ -17,14 +26,26 @@ class ViewController: UIViewController {
     // MARK: - Setup
     
     private func setupHierarchy() {
-        
+        view.addSubview(tableView)
     }
 
     private func setupLayout() {
-        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 
     // MARK: - Actions
     
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+}
