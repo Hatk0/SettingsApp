fd300888ab9fc3e2133b84b79bf185b7da292b76
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-04 10:40:25 +0300

    Create DetailView and make basic setup

---
+++ SettingsApp/Sources/DetailView.swift
@@ -0,0 +1,28 @@
+import UIKit
+import SnapKit
+
+class DetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+}
