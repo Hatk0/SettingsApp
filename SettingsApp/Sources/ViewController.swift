54d7b546059033b6343b4edfa357b7838fef7b7c
Dmitry Yastrebov <stewielime2018@gmail.com>
2023-10-04 10:16:58 +0300

    Register CustomTableViewCell in ViewController and add methods

--- SettingsApp/Sources/ViewController.swift
+++ SettingsApp/Sources/ViewController.swift
@@ -3,11 +3,13 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    private var settings: [[Setting]]?
+    
     // MARK: - Outlets
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.dataSource = self
         tableView.delegate = self
         return tableView
@@ -17,6 +19,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        settings = Setting.settings
         view.backgroundColor = .white
         title = "Settings"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -35,18 +38,37 @@ class ViewController: UIViewController {
             make.top.right.bottom.left.equalTo(view)
         }
     }
-
-    // MARK: - Actions
-    
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 45
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return settings?.count ?? 0
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+        return settings?[section].count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomTableViewCell
+        cell?.contentView.isUserInteractionEnabled = false
+        cell?.setting = settings?[indexPath.section][indexPath.row]
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if let setting = settings?[indexPath.section][indexPath.row] {
+            print("The setting is selected: \(setting.title)")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
 }
